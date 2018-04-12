27f8d5ff86f6bbb8f455fea0a0bcb38f3fa58d95
Garrett Honke <garretthonke@gmail.com>
2018-04-11 22:02:30 -0400

    edited setup.py to conform to d3m

--- setup.py
+++ setup.py
@@ -9,8 +9,15 @@ setup(name='croc',
                         'pandas >= 0.19.2',
                         'scipy >= 0.19.0',
                         'tesserocr >= 2.2.2',
-                        'pyenchant >= 2.0.0'
-                        ],
-
-      include_package_data=True,
+                        'spacy >= 2.0.9'
+                        'requests >= 2.18.4',
+                        'numpy >= 1.13.3'],
+      dependency_links=[
+            "git+https://github.com/NewKnowledge/croc-d3m-wrapper"
+      ],
+      entry_points={
+        'd3m.primitives': [
+            'distil.croc = CROCd3mWrapper:croc'
+        ],
+      },
       )
