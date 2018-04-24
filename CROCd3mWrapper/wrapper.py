56560c05871406de64375ab6a3db3a861723701c
Garrett Honke <garretthonke@gmail.com>
2018-04-24 15:48:11 -0400

    logging bug fixed (?)

--- CROCd3mWrapper/wrapper.py
+++ CROCd3mWrapper/wrapper.py
@@ -37,7 +37,7 @@ class Hyperparams(hyperparams.Hyperparams):
 class Croc(PrimitiveBase[Inputs, Outputs, Params, Hyperparams]):
     metadata = metadata_base.PrimitiveMetadata({
         # The same path the primitive is registered with entry points in setup.py.
-        'python_path': 'd3m.primitives.distil.simon',
+        'python_path': 'd3m.primitives.distil.croc',
         })
 
     def __init__(self, *, hyperparams: Hyperparams)-> None:
