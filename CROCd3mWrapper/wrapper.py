35145220e1567e9cca29a7fd6e71e5dfb60dfee4
Garrett Honke <garretthonke@gmail.com>
2018-04-25 19:42:43 -0400

    new design pattern matching simon wrapper

--- CROCd3mWrapper/wrapper.py
+++ CROCd3mWrapper/wrapper.py
@@ -1,18 +1,13 @@
 import io
 import os
-import time
-import re
-from PIL import Image, ImageFilter
 
 import requests
+import typing
 import spacy
-from json import dumps
-from tesserocr import PyTessBaseAPI
 import numpy as np
 import pandas as pd
-from keras.preprocessing import image
-from keras.applications.inception_v3 \
-    import decode_predictions, preprocess_input
+
+from nk_croc import *
 
 from d3m.primitive_interfaces.base import PrimitiveBase, CallResult
 
@@ -34,19 +29,43 @@ class Hyperparams(hyperparams.Hyperparams):
     pass
 
 
-class Croc(PrimitiveBase[Inputs, Outputs, Params, Hyperparams]):
+class croc(PrimitiveBase[Inputs, Outputs, Params, Hyperparams]):
     metadata = metadata_base.PrimitiveMetadata({
+        # # Simply an UUID generated once and fixed forever. Generated using "uuid.uuid4()".
+        # 'id': "d2fa8df2-6517-3c26-bafc-87b701c4043a",
+        'version': __version__,
+        'name': "croc",
+        # Keywords do not have a controlled vocabulary. Authors can put here whatever they find suitable.
+        'keywords': ['OCR', 'object detection', 'image analysis'],
+        'source': {
+            'name': __author__,
+            'uris': [
+                # Unstructured URIs.
+                "https://github.com/NewKnowledge/croc-d3m-wrapper",
+            ],
+        },
+        # A list of dependencies in order. These can be Python packages, system packages, or Docker images.
+        # Of course Python packages can also have their own dependencies, but sometimes it is necessary to
+        # install a Python package first to be even able to run setup.py of another package. Or you have
+        # a dependency which is not on PyPi.
+        'installation': [{
+            'type': metadata_module.PrimitiveInstallationType.PIP,
+            'package_uri': 'git+https://github.com/NewKnowledge/croc-d3m-wrapper.git@{git_commit}#egg=CROCd3mWrapper'.format(
+                git_commit=,
+            ),
+        }],
         # The same path the primitive is registered with entry points in setup.py.
         'python_path': 'd3m.primitives.distil.croc',
-        })
+        # Choose these from a controlled vocabulary in the schema. If anything is missing which would
+        # best describe the primitive, make a merge request.
+        'algorithm_types': [
+            metadata_module.PrimitiveAlgorithmType.MULTILABEL_CLASSIFICATION,
+        ],
+        'primitive_family': metadata_module.PrimitiveFamily.DIGITAL_IMAGE_PROCESSING,
+    })
 
     def __init__(self, *, hyperparams: Hyperparams)-> None:
         super().__init__(hyperparams=Hyperparams)
-
-        self.target_size = (299, 299)
-        self.model = InceptionV3(weights='imagenet')
-        self.nlp = spacy.load('en')
-        self.n_top_preds = 10
 
     def produce(self, *, inputs: Inputs) -> CallResult[Outputs]:
         """
@@ -55,7 +74,7 @@ class Croc(PrimitiveBase[Inputs, Outputs, Params, Hyperparams]):
 
         Parameters
         ----------
-        inputs : Image URI or filepath
+        inputs : Image URI or local filepath
 
         Returns
         -------
@@ -66,41 +85,9 @@ class Croc(PrimitiveBase[Inputs, Outputs, Params, Hyperparams]):
 
         image_path = inputs
 
-        try:
-            if validate_url(image_path):
-                filename = 'target_img.jpg'
-                load_image_from_web(image_path)
-            else:
-                filename = image_path
-        except:
-            return "Image loading failed."
+        image_analyzer = Croc()
 
-        try:
-            print('preprocessing image')
-            X = np.array(
-                [load_image(
-                    filename, self.target_size, prep_func=preprocess_input)])
-
-            print('making object predictions')
-            object_predictions = classify_objects(X, self.model,
-                                                  decode_predictions,
-                                                  self.n_top_preds)
-
-            object_predictions = pd.DataFrame.from_records(
-                object_predictions[0], columns=['id', 'label', 'confidence'])
-
-            print('performing character recognition')
-            char_predictions = char_detect(filename, self.nlp)
-
-            if filename == 'target_img.jpg':
-                os.remove('target_img.jpg')
-
-            return dumps(dict(
-                objects=object_predictions.to_dict(),
-                text=[str(i) for i in char_predictions['text']],
-                tokens=char_predictions['tokens']))
-        except:
-            return "Something went wrong when generating CROC predictions."
+        return image_analyzer.predict(inputs=image_path)
 
     def fit(self) -> None:
         pass
@@ -114,82 +101,9 @@ class Croc(PrimitiveBase[Inputs, Outputs, Params, Hyperparams]):
     def set_training_data(self, *, inputs: Inputs, outputs: Outputs) -> None:
         pass
 
-    def load_image(img_path, target_size, prep_func=lambda x: x):
-        ''' load image given path and convert to an array
-        '''
-        img = image.load_img(img_path, target_size=target_size)
-        x = image.img_to_array(img)
-        return prep_func(x)
-
-    def load_image_from_web(image_url):
-        ''' load an image from a provided hyperlink
-        '''
-        # get image
-        response = requests.get(image_url)
-        with Image.open(io.BytesIO(response.content)) as img:
-            # convert to jpeg
-            if img.format is not 'jpeg':
-                img = img.convert('RGB')
-            img.save('target_img.jpg')
-
-    def validate_url(url):
-        url_validator = re.compile(
-            r'^(?:http|ftp)s?://'  # http:// or https://
-            r'(?:(?:[A-Z0-9](?:[A-Z0-9-]{0,61}[A-Z0-9])?\.)+(?:[A-Z]{2,6}\.?|[A-Z0-9-]{2,}\.?)|' #domain...
-            r'localhost|'  # localhost...
-            r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})'  # ...or ip
-            r'(?::\d+)?'  # optional port
-            r'(?:/?|[/?]\S+)$', re.IGNORECASE)
-
-        return bool(url_validator.match(url))
-
-    def cleanup_text(raw_chars, nlp, logging=False):
-        ''' use spacy to clean text and find tokens
-        '''
-        doc = nlp(raw_chars, disable=['parser', 'ner'])
-        text = [t.text for t in doc]
-        tokens = [tok.lemma_.lower().strip() for tok in doc
-                  if tok.lemma_ != '-PRON-']
-        tokens = [tok for tok in tokens
-                  if tok not in nlp.Defaults.stop_words and
-                  tok not in string.punctuation]
-
-        return dict(tokens=list(set(tokens)), text=text)
-
-    def classify_objects(image_array, model, decode_func, n_top_preds):
-        ''' Returns binary array with ones where the model predicts that
-            the image contains an instance of one of the target classes
-            (specified by wordnet id)
-        '''
-        predictions = model.predict(images_array)
-        # decode the results into list of tuples (class, description, probability)
-        predictions = decode_func(predictions, top=n_top_preds)
-        return predictions
-
-    def char_detect(img_path, nlp):
-        ''' Run tesseract ocr on an image supplied
-            as an image path.
-        '''
-        with PyTessBaseAPI() as ocr_api:
-            with Image.open(img_path) as image:
-                # will need a better preprocessing approach here
-                # if we stay with tesseract:
-                sharp_image = image.filter(ImageFilter.SHARPEN)
-
-                ocr_api.SetImage(sharp_image)
-                raw_chars = ocr_api.GetUTF8Text()
-                # char_confs = ocr_api.AllWordConfidences()
-
-                text = cleanup_text(raw_chars, nlp)
-
-                # utf encode the clean raw output
-                clean_chars = [i.encode('utf-8') for i in text['text']]
-
-                return dict(tokens=text['tokens'], text=clean_chars)
-
 
 if __name__ == '__main__':
-    client = Croc(hyperparams={})
+    client = croc(hyperparams={})
     image_path = 'http://i0.kym-cdn.com/photos/images/facebook/001/253/011/0b1.jpg'
     result = client.produce(inputs=image_path)
     print(result)
--- setup.py
+++ setup.py
@@ -1,46 +1,20 @@
-import os
 from distutils.core import setup
-from setuptools.command.develop import develop
-from setuptools.command.install import install
-
-
-class PostDevelopCommand(develop):
-    """Post-installation for development mode."""
-    def run(self):
-        os.system("python3 -m spacy download en")
-        develop.run(self)
-
-
-class PostInstallCommand(install):
-    """Post-installation for installation mode."""
-    def run(self):
-        os.system("python3 -m spacy download en")
-        install.run(self)
 
 
 setup(name='CROCd3mWrapper',
       version='1.0.0',
       description='Character recognition and object classification system.',
       packages=['CROCd3mWrapper'],
-      install_requires=['Keras >= 2.0.2',
-                        'scikit-learn >= 0.18.1',
-                        'pandas >= 0.19.2',
-                        'scipy >= 0.19.0',
-                        'tesserocr >= 2.2.2',
-                        'spacy >= 2.0.9',
-                        'requests >= 2.18.4',
+      install_requires=['pandas >= 0.19.2',
                         'numpy >= 1.13.3',
-                        'Pillow >= 5.1.0'],
+                        'Pillow >= 5.1.0',
+                        'nk_croc == 1.0.0'],
       dependency_links=[
-                       "git+https://github.com/NewKnowledge/croc-d3m-wrapper.git#d3m"
+                       "git+https://github.com/NewKnowledge/nk_croc.git"
                        ],
       entry_points={
         'd3m.primitives': [
                           'distil.croc = CROCd3mWrapper:Croc'
                           ],
-                   },
-      cmdclass={
-        'develop': PostDevelopCommand,
-        'install': PostInstallCommand,
-               },
+                   }
       )
