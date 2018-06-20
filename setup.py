372b67af852530e73b26cc18d4985f6b32e4a469
Garrett Honke <garretthonke@gmail.com>
2018-06-20 19:54:28 -0400

    reformatted some stuff

--- setup.py
+++ setup.py
@@ -1,20 +1,23 @@
 from distutils.core import setup
 
 
-setup(name='CROCd3mWrapper',
-      version='1.2.2',
-      description='character recognition and object classification primitive.',
-      packages=['CROCd3mWrapper'],
-      keywords=['d3m_primitive'],
-      install_requires=['pandas >= 0.22.0, < 0.23.0',
-                        'numpy >= 1.13.3',
-                        'nk_croc >= 1.1.0'],
-      dependency_links=[
-                       "git+https://github.com/NewKnowledge/nk_croc@155be671f66978084055915ed582efbd38a66651#egg=nk_croc-1.1.0"
-                       ],
-      entry_points={
+setup(
+    name='CROCd3mWrapper',
+    version='1.2.2',
+    description='character recognition and object classification primitive.',
+    packages=['CROCd3mWrapper'],
+    keywords=['d3m_primitive'],
+    install_requires=[
+        'pandas >= 0.22.0, < 0.23.0',
+        'numpy >= 1.13.3',
+        'nk_croc >= 1.1.0'
+    ],
+    dependency_links=[
+        "git+https://github.com/NewKnowledge/nk_croc@155be671f66978084055915ed582efbd38a66651#egg=nk_croc-1.1.0"
+    ],
+    entry_points={
         'd3m.primitives': [
-                          'distil.croc = CROCd3mWrapper:croc'
-                          ],
-                   }
-      )
+            'distil.croc = CROCd3mWrapper:croc'
+        ],
+    }
+)
