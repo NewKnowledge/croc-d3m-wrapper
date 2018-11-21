3bbb361bb7a975aa86474f2b13195ac260165ac6
Numa Dhamani <numadhamani@gmail.com>
2018-11-21 14:48:01 -0600

    nk_croc to d3m_croc

--- setup.py
+++ setup.py
@@ -10,11 +10,11 @@ setup(
     install_requires=[
         'pandas >= 0.22.0, < 0.23.0',
         'numpy >= 1.13.3',
-        'nk_croc >= 1.1.1'
+        'd3m_croc >= 1.1.1'
     ],
     dependency_links=[
-        "git+https://github.com/NewKnowledge/nk_croc@b2dad8fcfbeb40e6f07e2cf4cec36c385fb45e73#egg=nk_croc-1.1.1"
-    ],
+        "git+https://github.com/NewKnowledge/d3m_croc@69db8b405bed681e05d556c7c9933439ab544686#egg=d3m_croc-1.1.1"
+    ], # pointing to the latest commit on nd/d3m_croc branch
     entry_points={
         'd3m.primitives': [
             'distil.croc = CROCd3mWrapper:croc'
