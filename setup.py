9533735f3a5c6bac8e76d354b734566eb17f515a
Numa Dhamani <numadhamani@gmail.com>
2019-03-22 12:45:31 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setup(
         'd3m_croc >= 1.1.1'
     ],
     dependency_links=[
-        "git+https://github.com/NewKnowledge/d3m_croc@2b76da226b912624e6ad4add7e588f5886fec185#egg=d3m_croc-1.1.1"
+        "git+https://github.com/NewKnowledge/d3m_croc@182c9f936fabfd89e8432dc004ff718f36e5aa14#egg=d3m_croc-1.1.1"
     ], 
     entry_points={
         'd3m.primitives': [
