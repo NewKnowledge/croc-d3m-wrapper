86695826d2481949c55e38020be222d231962806
Numa Dhamani <numadhamani@gmail.com>
2019-04-29 13:52:39 -0500

    Update install_requires

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ setup(
     install_requires=[
         'pandas == 0.23.4',
         'numpy >= 1.15.4',
-        'd3m_croc @ git+https://github.com/NewKnowledge/d3m_croc@32fbd001000574eac18aab7d4544dd72c8224948#egg=d3m_croc-1.1.1"
+        'd3m_croc @ git+https://github.com/NewKnowledge/d3m_croc@32fbd001000574eac18aab7d4544dd72c8224948#egg=d3m_croc-1.1.1'
     ], 
     entry_points={
         'd3m.primitives': [
